a2c5207e67c66288cc04a27a0ae96b7dd9758ccd
Sara <ssuarezlo@unal.edu.co>
2025-11-22 10:16:05 -0500

    Eliminacion de constantes mágicas

--- homework/src/_internals/wordcount.py
+++ homework/src/_internals/wordcount.py
@@ -9,8 +9,11 @@ from .write_count_words import write_count_words
 
 
 def main():
+    input_folder = "data/input/"
+    output_folder = "data/output/"
+
     ## read all lines
-    all_lines = read_all_lines()
+    all_lines = read_all_lines(input_folder)
 
     ### preprocess lines
     all_lines = preprocess_lines(all_lines)
@@ -21,16 +24,7 @@ def main():
     ### count words
     counter = count_words(words)
 
-    # count the frequency of the words in the files in the input directory
-    # counter = {}
-    # for filename in input_files_list:
-    #     with open("data/input/" + filename) as f:
-    #         for l in f:
-    #             for w in l.split():
-    #                 w = w.lower().strip(",.!?")
-    #                 counter[w] = counter.get(w, 0) + 1
-
-    write_count_words(counter)
+    write_count_words(counter, output_folder)
 
 
 if __name__ == "__main__":
