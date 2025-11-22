5bb7f386173b441d74da384931521dab280a5255
Sara <ssuarezlo@unal.edu.co>
2025-11-22 11:37:54 -0500

    final v1

---
+++ homework/__main__.py
@@ -0,0 +1,9 @@
+### Función main.py
+"""Entry point for the homework package."""
+
+# c
+
+from homework.src.wordcount import main
+
+if __name__ == "__main__":
+    main()
---
+++ homework/src/wordcount.py
@@ -0,0 +1,38 @@
+# obtain a list of files in the input directory
+import sys
+
+from homework.src._internals.count_words import count_words
+from homework.src._internals.preprocess_lines import preprocess_lines
+from homework.src._internals.read_all_lines import read_all_lines
+from homework.src._internals.split_into_words import split_into_words
+
+from ._internals.write_word_counts import write_count_words
+
+
+def main():
+
+    if len(sys.argv) != 3:
+        print("Usage: python3 -m homework <input_folder> <output_folder>")
+        # sys.exit(1)
+    return
+
+    # input_folder = sys.argv[1]
+    # output_folder = sys.argv[2]
+
+    ## read all lines
+    all_lines = read_all_lines(input_folder)
+
+    ### preprocess lines
+    all_lines = preprocess_lines(all_lines)
+
+    ### split in words
+    words = split_into_words(all_lines)
+
+    ### count words
+    counter = count_words(words)
+
+    write_count_words(counter, output_folder)
+
+
+if __name__ == "__main__":
+    main()
